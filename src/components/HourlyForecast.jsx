50b9c97f3a56bd67c6754c7302970fe7eba52c81
pr4dyumn4 <pradyumnajekumar@gmail.com>
2026-01-30 12:20:53 +0530

    css modified 2

--- src/components/HourlyForecast.jsx
+++ src/components/HourlyForecast.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import HourCard from './HourCard';
 function HourlyForecast({data}) {
-    return (<div className=" flex max-w-[800px] gap-4 p-4 rounded-2xl  items-center">
+    return (<div className=" flex max-w-200 gap-4 p-4 rounded-2xl  items-center">
         <div className="flex gap-3 overflow-x-auto pb-4 scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-transparent snap-x snap-mandatory">
             {data.map((hour, index) => (
                 <HourCard key={index} hour={hour} />
