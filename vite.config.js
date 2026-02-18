c989322d9325a820f8083d3397823eef3456f4ec
pr4dyumn4 <pradyumnajekumar@gmail.com>
2026-02-18 16:08:10 +0530

    implemented API

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/weather': {  // Proxy /weather/* to backend
-        target: 'https://weather-api-production-f78d.up.railway.app',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
+  server: [],
 })
