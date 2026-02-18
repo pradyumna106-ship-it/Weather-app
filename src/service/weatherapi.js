36d0d9d93413aae606040683f5e08f57eecbd280
pr4dyumn4 <pradyumnajekumar@gmail.com>
2026-02-18 16:15:06 +0530

    API Changed

--- src/service/weatherapi.js
+++ src/service/weatherapi.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
+const api = axios.create({
+  baseURL: "https://weather-api-production-f78d.up.railway.app"
+});
+
 export const weather = async(city) => {
     try{
-        const { data } = await axios.get(`https://weather-api-production-f78d.up.railway.app/weather/london/weather/${city}`);  // Proxy works!
+        const { data } = await api.get(`/weather/${city}`)  // Proxy works!
         return data;
     } catch(e) {
         console.error(e);
